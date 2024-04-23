766f465be2b8df5fdfa36ea939ba9283f9ad9b6c
Jessica <67671717+jessicale321@users.noreply.github.com>
2024-04-23 14:12:08 -0700

    Files for build

---
+++ RUN_SIM.bat
@@ -0,0 +1,1 @@
+NewUnityProject.exe --config "C:/Program Files/MiddleVR/Improov/data/Config/Cube/VisCube-M4-1920c-mono-ART-MiddleVR_DriveSim_Upgrades.vrx" -popupwindow -force-d3d11 --mvr-window-geometry="1920x1200+1920+0"
