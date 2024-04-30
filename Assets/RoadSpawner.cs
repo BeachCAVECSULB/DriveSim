130d6547da9148541e7b2dbb5d8bc75a6c490625
jessicale321 <jessicale321@gmail.com>
2024-04-30 00:25:10 -0700

    Generative straight road in StraightRoad scene

---
+++ Assets/RoadSpawner.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadSpawner : MonoBehaviour
+{
+    public GameObject roadSectionPrefab;
+    private bool enteredSpawnTrigger = false;
+    private Vector3 nextSpawnPoint = new Vector3(0, 0, 120);
+
+    private GameObject previousRoadSection;
+    private GameObject startRoad;
+
+    private void Start()
+    {
+        startRoad = Instantiate(roadSectionPrefab, new Vector3(0, 0 ,0), Quaternion.identity);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!enteredSpawnTrigger && other.gameObject.CompareTag("Trigger"))
+        {
+            enteredSpawnTrigger = true;
+            SpawnRoadSection();
+            previousRoadSection = other.transform.parent.gameObject;
+        }
+    }
+
+    private void SpawnRoadSection()
+    {
+        if (previousRoadSection != null)
+        {
+            Destroy(previousRoadSection);
+        }
+        else
+        {
+            previousRoadSection = startRoad;
+        }
+
+        GameObject newRoadSection = Instantiate(roadSectionPrefab, nextSpawnPoint, Quaternion.identity);
+        nextSpawnPoint += new Vector3(0, 0, 120);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+       if (other.gameObject.CompareTag("Trigger"))
+        {
+            enteredSpawnTrigger = false;
+        }
+    }
+}
