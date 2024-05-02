8026159de83f7fecb57862eb313cd6756100b352
jessicale321 <jessicale321@gmail.com>
2024-05-02 09:45:10 -0700

    Popup text when pressing U

---
+++ Assets/UIManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private Transform panel;
+    private bool panelIsActive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        panel.gameObject.SetActive(false);
+        panelIsActive = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            if (panelIsActive == false)
+            {
+                panel.gameObject.SetActive(true);
+                panelIsActive = true;
+            }
+            else
+            {
+                panel.gameObject.SetActive(false);
+                panelIsActive = false;
+            }
+        }
+    }
+}
